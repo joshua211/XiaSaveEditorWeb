1ff90239f6b44fe5b844d1e97f073e0c958b91ff
joshua211 <lycosh211@gmx.de>
2020-12-02 18:32:49 +0100

    add fa and basic editor ui

--- Service/SaveGameService.cs
+++ Service/SaveGameService.cs
@@ -1,9 +1,25 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
 using XiaSaveFileLib;
 
 namespace XiaSaveEditorWeb.Service
 {
     public class SaveGameService
     {
-        public SaveFile SaveFile { get; set; }
+        public event Action OnLoadComplete;
+        public SaveFile SaveFile { get; private set; }
+
+        public async Task LoadSaveFile(Stream stream, string fileName)
+        {
+            //Copy to new stream bc FileInput does not allow sync read
+            var mem = new MemoryStream();
+            await stream.CopyToAsync(mem);
+            await Task.Yield();
+            mem.Position = 0;
+            SaveFile = await SaveFileManager.LoadSaveFileAsync(mem, fileName);
+            System.Console.WriteLine(SaveFile.Header.FileName + " was loaded");
+            OnLoadComplete?.Invoke();
+        }
     }
 }
